3768c9b9715cac1668b4c6277251699fb987d13c
William9923 <williamong1400@gmail.com>
2019-09-19 21:02:07 +0700

    Baca File debugged

--- BacaFile.java
+++ BacaFile.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class BacaFile {
 
@@ -14,7 +15,8 @@ public class BacaFile {
 	*/
 
 	/*Konstruktor*/
-	public BacaFile (String filename) {
+	public BacaFile (String filename, int opsiBaca) {
+		this.opsiBaca = opsiBaca;
 		this.filename = filename;
 	}
 
@@ -31,21 +33,136 @@ public class BacaFile {
 
 	// }
 
-	public void inisiasiBacaFile() {
+	public static int[] jumlahData(String filename) {
+		/*Mengembalikan panjang data dengan dimensi*/
+		/*Nilai pertama mengembalikan nilai panjang file*/
+		/*Nilai kedia mengembalikan nilai lebar file*/
+
 		try {
-			FileReader fileReader = new FileReader(filename);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			File file = new File (filename + ".txt");
+
+			Scanner scanFileChecker = new Scanner(file);
 
 			// do all in here
+			int nLine = 0;
+			String s = "";
+			while (scanFileChecker.hasNextLine()) {
+				nLine += 1;
+				s = scanFileChecker.nextLine();
+			}
+			if (nLine > 0) {
+				String[] sSplit = s.split(" ");
+				int[] val = {nLine, sSplit.length};
+				return val;
+			}
+			int[] val ={0,0};
+			return val;
 
-			bufferedReader.close();
 		} catch (FileNotFoundException ex) {
 			System.out.println("File tidak ditemukan");
-		} catch ( IOException e) {
-			System.out.println("Error");
+			int[] val = {0, 0};
+			return val;
+		}
+	}
+
+	/*Dilakukan  sesuai opsi*/
+	public Matrix inputMatrix() {
+		int[] jumlahData = BacaFile.jumlahData(filename);
+
+		double[][] arr = new double[jumlahData[0]][jumlahData[1]];
+
+		try {
+			File file = new File(filename + ".txt");
+
+			Scanner scanner = new Scanner(file);
+
+			String s;
+			String[] sSplit;
+			double[] arrTemp = new double[jumlahData[1]];
+			int count = 0;
+
+			while (scanner.hasNextLine()) {
+				System.out.println("Debug");
+				s = scanner.nextLine();
+				System.out.println(s);
+				sSplit = s.split(" ");
+				for (int i = 0; i < jumlahData[1]; i++) {
+					arrTemp[i] = Double.parseDouble(sSplit[i]);
+
+				}
+				// gunakan metode kopi array yang efektif
+				arr[count] = Arrays.copyOf(arrTemp, jumlahData);
+				count++;
+			}
+			Matrix matrix = new Matrix(arr);
+			return matrix;
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File tidak ditemukan");
+
+			Matrix matrix = new Matrix(arr);
+			return matrix;
 		}
 
-		/*Dilakukan  sesuai opsi*/
+	}
+
+	public double[][] inputTitik() {
+		int[] jumlahData = BacaFile.jumlahData(filename);
+		int banyakTitik = jumlahData[0];
+
+		double[][] arr = new double[2][banyakTitik];
+
+		try {
+			File file = new File(filename + ".txt");
+
+			Scanner scanner = new Scanner(file);
+
+			String s;
+			String[] sSplit;
+			double[] arrX = new double[banyakTitik];
+			double[] arrY = new double[banyakTitik];
+
+			int i = 0;
+
+			while (scanner.hasNextLine()) {
+				s = scanner.nextLine();
+				sSplit = s.split(" ");
+				arrX[i] = Double.parseDouble(sSplit[0]);
+				arrY[i] = Double.parseDouble(sSplit[1]);
+
+				i++;
+			}
+
+			arr[0] = arrX; arr[1] = arrY;
+			return arr;
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File tidak ditemukan");
+			return arr;
+
+		}
+	}
+
+
+	public static void main(String[] args) {
+		BacaFile file1 = new BacaFile("test1",1);
+		BacaFile file2 = new BacaFile("test2",2);
+
+		Matrix matrix = file1.inputMatrix();
+		matrix.show();
+
+		System.out.println("============================================================================");
+
+		double arr[][] = file2.inputTitik();
+		double[] arrX = arr[0];
+		double[] arrY = arr[1];
+
+		for (int i = 0; i < arrX.length; i++){
+			System.out.printf("%.2f %.2f\n",arrX[i],arrY[i]);
+		}
+		
+
+
 
 	}
 }
